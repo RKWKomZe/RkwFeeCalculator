69a848fe703aefdf73635bccf445bf986cc9e761
Christian Dilger <c.dilger@addorange.de>
2019-06-13 15:03:48 +0200

    [CHANGE] cleanup on controller

--- Classes/Controller/CalculationController.php
+++ Classes/Controller/CalculationController.php
@@ -37,8 +37,6 @@ class CalculationController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionCont
     public function showAction(\Rkw\RkwFeecalculator\Domain\Model\Calculation $calculation = null)
     {
 
-        $this->initializeAction();
-
         if (! $calculation) {
             $calculation = new Calculation();
             $calculation->setCalculator($this->calculatorRepository->findByUid($this->settings['calculator']));
@@ -56,7 +54,7 @@ class CalculationController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionCont
      * action store
      *
      * @validate $calculation \Rkw\RkwFeecalculator\Validation\CalculationValidator
-     * @param Calculator|null $calculator
+     * @param Calculation|null $calculation
      * @throws \TYPO3\CMS\Extbase\Mvc\Exception\StopActionException
      * @throws \TYPO3\CMS\Extbase\Mvc\Exception\UnsupportedRequestTypeException
      */
@@ -69,50 +67,4 @@ class CalculationController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionCont
         $this->forward('show', null, null, array('calculation' => $calculation));
 
     }
-
-    /**
-     * A special action which is called if the originally intended action could
-     * not be called, for example if the arguments were not valid.
-     *
-     * The default implementation sets a flash message, request errors and forwards back
-     * to the originating action. This is suitable for most actions dealing with form input.
-     *
-     * We clear the page cache by default on an error as well, as we need to make sure the
-     * data is re-evaluated when the user changes something.
-     *
-     * @return string
-     */
-//    protected function errorAction() {
-//        echo 'ErrorAction';
-//        $this->clearCacheOnError();
-//        echo '<pre>';
-//        foreach ($this->arguments->getValidationResults()->getFlattenedErrors() as $propertyPath => $errors) {
-//            foreach ($errors as $error) {
-//                $message .= 'Error for ' . $propertyPath . ': ' . $error->render() .
-//                    PHP_EOL;
-//            }
-//            echo 'Error: ' . $message;
-//        }
-//        echo '</pre>';
-//
-//        $errorFlashMessage = $this->getErrorFlashMessage();
-//        if ($errorFlashMessage !== FALSE) {
-//            $errorFlashMessageObject = new \TYPO3\CMS\Core\Messaging\FlashMessage(
-//                $errorFlashMessage,
-//                '',
-//                \TYPO3\CMS\Core\Messaging\FlashMessage::ERROR
-//            );
-//            $this->controllerContext->getFlashMessageQueue()->enqueue($errorFlashMessageObject);
-//        }
-//        $referringRequest = $this->request->getReferringRequest();
-//        if ($referringRequest !== NULL) {
-//            $originalRequest = clone $this->request;
-//            $this->request->setOriginalRequest($originalRequest);
-//            $this->request->setOriginalRequestMappingResults($this->arguments->getValidationResults());
-//            $this->forward($referringRequest->getControllerActionName(), $referringRequest->getControllerName(), $referringRequest->getControllerExtensionName(), $referringRequest->getArguments());
-//        }
-//
-//        $message = 'An error occurred while trying to call ' . get_class($this) . '->' . $this->actionMethodName . '().' . PHP_EOL;
-//        return $message;
-//    }
 }
